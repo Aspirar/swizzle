20fb849364089e8875fd354af6f8cae5cebc283c
Lakshya Gupta <theultimateaim@gmail.com>
2021-07-31 13:00:25 +0530

    Background and text color

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,15 @@
 <template>
   <router-view/>
 </template>
+
+<style>
+:root {
+  --app-bg-col: black;
+  --app-text-col: white;
+}
+
+body {
+  background-color: var(--app-bg-col);
+  color: var(--app-text-col);
+}
+</style>
